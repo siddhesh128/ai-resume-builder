308bde691071b65b3c05f3aa2756df350fbc953b
siddhesh128 <119682823+siddhesh128@users.noreply.github.com>
2024-07-29 00:19:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  mode: "development",
+  build: {
+    minify: false,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
